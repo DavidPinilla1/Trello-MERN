f95494adc39a205d789389aa73e0cc8c0c460994
DavidPinilla1 <metroid_300@hotmail.com>
2019-04-14 17:43:00 +0200

    added renderItems method and lyfecicle action
    the renderItems renders the BoardItems and the action dispatched brings the boards from the server

--- frontend/src/containers/Home/Home.jsx
+++ frontend/src/containers/Home/Home.jsx
@@ -2,30 +2,39 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './Home.scss';
 import BoardItem from '../../components/UI/BoardItem.jsx'
+import {newBoard, getboards} from '../../store/actions/board'
 class Home extends Component {
   componentWillMount() {
-    // this.props.dispatch(getboards(1, 0, 'desc'));
+    this.props.dispatch(getboards());
   }
-
+  
   renderItems=(boards)=>(
-    boards.list ? 
-      boards.list.map(item =>(
-        <BoardItem {...item} key={item._id}/>
+    boards ? 
+      boards.map(item =>(
+        <BoardItem item={item} key={item._id} />
       ))
     :null
   )
+  handleKeyUp=(ev)=>{
+    if (ev.keyCode === 13) {
+      this.props.dispatch(newBoard(ev.target.value))
+      ev.target.value=''
+    }
+  }
   render() {
+   console.log(this.props)
     return (
       <div className="Home">
-      {/* this.renderItems(this.props.boards) */}
-       <button className="newBoard">Add new Board</button>
+      <div className="BoardItems">{this.props.board.boards? this.renderItems(this.props.board.boards):''}
+       <input type="text" className="newBoard" onKeyUp={this.handleKeyUp} placeholder="Add new Board" />
+       </div>
       </div>
     );
   }
 }
 function mapStateToProps(state) {
   return {
-    boards: state.boards
+    board: state.board
   };
 }
 export default connect(mapStateToProps)(Home);
