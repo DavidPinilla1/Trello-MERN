21d890a2b611fe315600037224898acfb258949f
DavidPinilla1 <metroid_300@hotmail.com>
2019-04-16 09:38:12 +0200

    Added header icons

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './Header.scss';
-
+import { NavLink, Link } from 'react-router-dom'
 class Header extends Component {
   render() {
     return (
       <div className="Header">
-       
+      <Link to="/"> <img className="logo" src="./images/trello.png" alt="logo"/></Link>
+        <div className="userlinks">
+        <NavLink className="links" to="/login"><img src="images/login.png" alt="login image"/> Login</NavLink>
+        <NavLink className="links" to="/register"><img src="./images/register.png" alt="register image"/> Register</NavLink>
+        </div>
       </div>
     );
   }
