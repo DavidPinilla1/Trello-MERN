d2f8a9779153f64333764d36d7dd385d3080ab8d
DavidPinilla1 <metroid_300@hotmail.com>
2019-04-18 10:26:41 +0200

    added USER reducer cases

--- frontend/src/store/reducers/user.js
+++ frontend/src/store/reducers/user.js
@@ -1,5 +1,9 @@
 export default function(state = {}, action) {
     switch (action.type) {
+      case 'LOGIN_FULFILLED':
+      return{...state,"userLogged":action.payload}
+      case 'REGISTER_FULFILLED':
+      return{...state,"userRegister":action.payload}
       default:
         return state;
     }
