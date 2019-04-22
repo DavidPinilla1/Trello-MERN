cfd5471136191fb2f6363a7abfe0b9b57972e17e
DavidPinilla1 <metroid_300@hotmail.com>
2019-04-22 16:22:44 +0200

    Added handleKeyUp function to the TaskInput to create new Tasks

--- frontend/src/components/UI/ListItem/ListItem.jsx
+++ frontend/src/components/UI/ListItem/ListItem.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
-import './ListItem.scss'
-const ListItem = props => {
+import './ListItem.scss';
+import {updateLists} from '../../../store/actions/board';
+import TaskItem from '../TaskItem/TaskItem.jsx';
+import {connect} from 'react-redux'
+const ListItem = props => { 
+  let board = props.board.currentBoard;
+   const renderTasks = List =>(
+  List.Tasks && List.Tasks.map(item =>(
+      <TaskItem item={item} key={item._id} List={List}/>
+    ))
+  )
+  const handleKeyUp = (ev) => {
+    if (ev.keyCode === 13) {
+      let newTask={
+        name:ev.target.value.trim(),
+        subTasks:[]
+      }
+      props.item.Tasks.push(newTask)
+      console.log(props)
+      props.dispatch(updateLists(board._id,board.lists))
+      ev.target.value = ''
+    }
+  }
   return (
       <div className="ListItem">
       <p className="List_header">
         {props.item.name}
       </p>
-      <input className="TaskInput" type="text" placeholder="Add new Task"
+      <div className="TasksItems">{props.item && renderTasks(props.item)}</div>
+      <input className="TaskInput" type="text" placeholder="Add new Task" onKeyUp={handleKeyUp}
       onFocus={e=>e.target.placeholder = ''} onBlur={e=>e.target.placeholder = "Add new Task"}/>
       </div>
   );
 };
-export default ListItem;
+function mapStateToProps(state) {
+  return {
+    board: state.board
+  };
+}
+export default connect(mapStateToProps)(ListItem);
