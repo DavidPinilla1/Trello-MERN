08e04995d408b9f143bcac46cc3e72776850daf6
DavidPinilla1 <metroid_300@hotmail.com>
2019-04-23 17:08:38 +0200

    board reducer updated to new changes in action generators

--- frontend/src/store/reducers/board.js
+++ frontend/src/store/reducers/board.js
@@ -1,18 +1,18 @@
 export default function(state = {}, action) {
     switch (action.type) {
-      case 'GET_BOARDS_FULFILLED':
+      case 'GET_BOARDS':
       return{...state, boards:action.payload}
      
-      case 'GET_BOARD_BY_ID_FULFILLED':
+      case 'GET_BOARD_BY_ID':
       return{...state,"currentBoard":action.payload}
 
       case 'CLEAR_BOARD':
       return{...state,"currentBoard":{}}
 
-      case 'UPDATE_LISTS_FULFILLED':
+      case 'UPDATE_LISTS':
       return{...state,"currentBoard":action.payload}
 
-      case 'ADD_BOARD_FULFILLED':
+      case 'ADD_BOARD':
       return{...state,"boards":[...state.boards,action.payload]}
       default:
         return state;
